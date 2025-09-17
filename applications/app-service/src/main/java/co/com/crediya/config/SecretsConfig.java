ba689ceaab9efd026064bee73ea8fca42d383132
DANIEL PEREZ VITOLA <dapevi97@gmail.com>
2025-09-16 21:51:30 -0500

    feat: se agrega secret manager

---
+++ applications/app-service/src/main/java/co/com/crediya/config/SecretsConfig.java
@@ -0,0 +1,38 @@
+package co.com.crediya.config;
+
+import co.com.bancolombia.secretsmanager.api.GenericManagerAsync;
+import co.com.bancolombia.secretsmanager.api.exceptions.SecretException;
+import co.com.bancolombia.secretsmanager.config.AWSSecretsManagerConfig;
+import co.com.bancolombia.secretsmanager.connector.AWSSecretManagerConnectorAsync;
+import co.com.crediya.api.seguridad.util.JwtProperties;
+import co.com.crediya.config.dto.JwtSecretDto;
+import software.amazon.awssdk.regions.Region;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecretsConfig {
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Bean
+    public JwtProperties jwtProperties(GenericManagerAsync secretManager) throws SecretException {
+        JwtSecretDto secret = secretManager.getSecret("jwt", JwtSecretDto.class).block();
+        assert secret != null;
+        return new JwtProperties(secret.getJwtSecretValue(), expiration);
+    }
+
+  @Bean
+  public GenericManagerAsync getSecretManager(@Value("${aws.region}") String region) {
+    return new AWSSecretManagerConnectorAsync(getConfig(region));
+  }
+
+  private AWSSecretsManagerConfig getConfig(String region) {
+    return AWSSecretsManagerConfig.builder()
+      .region(Region.of(region))
+      .cacheSize(5)
+      .cacheSeconds(3600)
+      .build();
+  }
+}
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/seguridad/util/JwtProperties.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/seguridad/util/JwtProperties.java
@@ -1,8 +1,5 @@
 package co.com.crediya.api.seguridad.util;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "jwt")
 public record JwtProperties(
         String secret,
         Long expiration
