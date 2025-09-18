2d67d05538aacad057968c82de4aa59881d0a4fc
DANIEL PEREZ VITOLA <dapevi97@gmail.com>
2025-09-18 18:34:00 -0500

    fix: se arregla security openapi

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/seguridad/SecurityConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/seguridad/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
                 })
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/api/v1/login/**").permitAll()
-                        .pathMatchers("/api/v1/reportes/swagger-docs/**", "/api-docs/**", "/webjars/**", "/swagger-ui/**").permitAll()
+                        .pathMatchers("/api/v1/reportes/swagger-docs/**", "/api/v1/reportes/api-docs/**", "/webjars/**", "/api/v1/reportes/swagger-ui/**").permitAll()
                         .pathMatchers("/actuator/health").permitAll()
                         .pathMatchers(HttpMethod.GET, rutas.getReportes()).hasAnyRole(ADMIN.name())
                         .anyExchange().authenticated()
